97eae099013826e1f81b878abc0cf9df0ac37ffa
Iryna Hromska <irahromska@gmail.com>
2025-09-04 12:43:12 +0300

    Fix auth urls

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/logout/", auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("kitchen.urls")),
     path("users/", include("users.urls")),
