972d809a9f967ba94006b49f784ba01f270fb2bc
Iryna Hromska <irahromska@gmail.com>
2025-09-04 13:24:38 +0300

    fixx

--- kitchen_service/settings/base.py
+++ kitchen_service/settings/base.py
@@ -113,3 +113,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
